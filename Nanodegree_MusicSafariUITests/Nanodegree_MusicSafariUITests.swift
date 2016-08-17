116b44b01962ba5ce0c05fdecd53ccbb4ad51ab4
frank-yuan <felk2005@gmail.com>
2016-08-17 15:58:35 +0800

    Initial Commit

---
+++ Nanodegree_MusicSafariUITests/Nanodegree_MusicSafariUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Nanodegree_MusicSafariUITests.swift
+//  Nanodegree_MusicSafariUITests
+//
+//  Created by Xuan Yuan (Frank) on 8/17/16.
+//  Copyright © 2016 frank-yuan. All rights reserved.
+//
+
+import XCTest
+
+class Nanodegree_MusicSafariUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
